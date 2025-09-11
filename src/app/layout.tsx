faad4323cbcecc862314d06fe4ce9ea44da08e14
Ifeanyichukwu Onyekwelu <ifeanyionyekwelu786@gmail.com>
2025-09-11 22:25:26 +0100

    Updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "BuiltByIfeanyi - Developer Portfolio",
   description: "Welcome to my developer portfolio!",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({
